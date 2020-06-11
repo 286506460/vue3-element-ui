72315858565d30da32b94e468417a870e3785d1f
sy <286506460@qq.com>
2020-06-11 22:22:16 +0800

    feat: add element-ui utils

---
+++ src/utils/clickoutside.ts
@@ -0,0 +1,70 @@
+// import * as Vue from 'vue'
+import { on } from './dom'
+
+const nodeList: any[] = []
+const ctx = '@@clickoutsideContext'
+
+let startClick: any
+let seed = 0
+
+on(document, 'mousedown', e => (startClick = e))
+
+on(document, 'mouseup', e => {
+	nodeList.forEach(node => node[ctx].documentHandler(e, startClick))
+})
+
+function createDocumentHandler(el: any, binding: any, vnode: any) {
+	return function(mouseup: any = {}, mousedown: any = {}) {
+		if (
+			!vnode ||
+			!mouseup.target ||
+			!mousedown.target ||
+			el.contains(mouseup.target) ||
+			el.contains(mousedown.target) ||
+			el === mouseup.target
+		)
+			return
+
+		if (binding.expression && el[ctx].methodName && vnode.context[el[ctx].methodName]) {
+			vnode.context[el[ctx].methodName]()
+		} else {
+			el[ctx].bindingFn && el[ctx].bindingFn()
+		}
+	}
+}
+
+/**
+ * v-clickoutside
+ * @desc 点击元素外面才会触发的事件
+ * @example
+ * ```vue
+ * <div v-element-clickoutside="handleClose">
+ * ```
+ */
+export default {
+	beforeMount(el: any, binding: any, vnode: any) {
+		nodeList.push(el)
+		const id = seed++
+		el[ctx] = {
+			id,
+			documentHandler: createDocumentHandler(el, binding, vnode),
+			methodName: binding.expression,
+			bindingFn: binding.value
+		}
+	},
+	updated(el: any, binding: any, vnode: any) {
+		el[ctx].documentHandler = createDocumentHandler(el, binding, vnode)
+		el[ctx].methodName = binding.expression
+		el[ctx].bindingFn = binding.value
+	},
+	unmounted(el: any) {
+		let len = nodeList.length
+		for (let i = 0; i < len; i++) {
+			if (nodeList[i][ctx].id === el[ctx].id) {
+				nodeList.splice(i, 1)
+				break
+			}
+		}
+		delete el[ctx]
+	}
+}
---
+++ src/utils/vue-popper.ts
@@ -0,0 +1,228 @@
+import {
+	ref,
+	watch,
+	defineComponent,
+	reactive,
+	nextTick,
+	onBeforeUnmount,
+	onDeactivated
+} from 'vue'
+import { PopupManager } from './popup'
+
+const PopperJS = require('./popper')
+const stop = (e: any) => e.stopPropagation()
+
+/**
+ * @param {HTMLElement} [reference=$refs.reference] - The reference element used to position the popper.
+ * @param {HTMLElement} [popper=$refs.popper] - The HTML element used as popper, or a configuration used to generate the popper.
+ * @param {String} [placement=button] - Placement of the popper accepted values: top(-start, -end), right(-start, -end), bottom(-start, -end), left(-start, -end)
+ * @param {Number} [offset=0] - Amount of pixels the popper will be shifted (can be negative).
+ * @param {Boolean} [visible=false] Visibility of the popup element.
+ * @param {Boolean} [visible-arrow=false] Visibility of the arrow, no style.
+ */
+interface VuePopperMixins {
+	popperElm: any
+	$refs: any
+	referenceElm: any
+	_popper: any
+	_reference: any
+	$slots: any
+	$emit: any
+}
+
+export default (
+	emit: any,
+	props: any,
+	{ popperElm, $refs, referenceElm, _popper, _reference, $slots, $emit }: VuePopperMixins
+) => {
+	// props
+	const transformOrigin = ref<boolean | string>(true)
+	const placement = ref<string>('bottom')
+	const boundariesPadding = ref<number>(5)
+	const reference = reactive<any>({})
+	const popper = reactive<any>({})
+	const offset = ref<number>(0)
+	const value = ref<boolean>(false)
+	const visibleArrow = ref<boolean>(false)
+	const arrowOffset = ref<number>(35)
+	const appendToBody = ref<boolean>(true)
+	const popperOptions = reactive<any>({ gpuAcceleration: false })
+	// Data
+	const showPopper = ref<boolean>(false)
+	const currentPlacement = ref<string>('')
+	// watch
+	watch(value, val => {
+		showPopper.value = val
+		emit('input', val)
+	})
+
+	watch(showPopper, val => {
+		if (props.disabled) return
+		val
+			? updatePopper({
+					popperElm,
+					$refs,
+					referenceElm,
+					_popper,
+					_reference,
+					$slots,
+					$emit
+			  })
+			: destroyPopper()
+		emit('input', val)
+	})
+
+	let popperJS: any
+	const appended = ref<boolean>(false)
+	// methods
+	const createPopper = ({
+		popperElm,
+		$refs,
+		referenceElm,
+		_popper,
+		_reference,
+		$slots,
+		$emit
+	}: VuePopperMixins) => {
+		currentPlacement.value = currentPlacement.value || placement.value
+		if (!/^(top|bottom|left|right)(-start|-end)?$/g.test(currentPlacement.value)) {
+			return
+		}
+
+		const options = popperOptions
+		const popper = (popperElm = popperElm || _popper || $refs.popper)
+		let reference = (referenceElm = referenceElm || _reference || $refs.reference)
+
+		if (!reference && $slots.reference && $slots.reference[0]) {
+			reference = referenceElm = $slots.reference[0].elm
+		}
+
+		if (!popper || !reference) return
+		if (visibleArrow.value) appendArrow(popper)
+		if (appendToBody.value) document.body.appendChild(popperElm)
+		if (popperJS && popperJS.destroy) {
+			popperJS.destroy()
+		}
+
+		options.placement = currentPlacement.value
+		options.offset = offset.value
+		options.arrowOffset = arrowOffset.value
+		popperJS = new PopperJS(reference, popper, options)
+		popperJS.onCreate((_: any) => {
+			$emit('created', {})
+			resetTransformOrigin()
+			nextTick(
+				updatePopper({
+					popperElm,
+					$refs,
+					referenceElm,
+					_popper,
+					_reference,
+					$slots,
+					$emit
+				})
+			)
+		})
+		if (typeof options.onUpdate === 'function') {
+			popperJS.onUpdate(options.onUpdate)
+		}
+		popperJS._popper.style.zIndex = PopupManager.nextZIndex()
+		popperElm.addEventListener('click', stop)
+	}
+
+	const updatePopper = ({
+		popperElm,
+		$refs,
+		referenceElm,
+		_popper,
+		_reference,
+		$slots,
+		$emit
+	}: VuePopperMixins): any => {
+		// const popperJS = this.popperJS
+		if (popperJS) {
+			popperJS.update()
+			if (popperJS._popper) {
+				popperJS._popper.style.zIndex = PopupManager.nextZIndex()
+			}
+		} else {
+			createPopper({
+				popperElm,
+				$refs,
+				referenceElm,
+				_popper,
+				_reference,
+				$slots,
+				$emit
+			})
+		}
+	}
+
+	const doDestroy = (forceDestroy: any) => {
+		/* istanbul ignore if */
+		if (!popperJS || (showPopper.value && !forceDestroy)) return
+		popperJS.destroy()
+		popperJS = null
+	}
+
+	const destroyPopper = () => {
+		if (popperJS) {
+			resetTransformOrigin()
+		}
+	}
+
+	const resetTransformOrigin = () => {
+		if (!transformOrigin.value) return
+		let placementMap: any = {
+			top: 'bottom',
+			bottom: 'top',
+			left: 'right',
+			right: 'left'
+		}
+		let placement = popperJS._popper.getAttribute('x-placement').split('-')[0]
+		let origin = placementMap[placement]
+		popperJS._popper.style.transformOrigin =
+			typeof transformOrigin.value === 'string'
+				? transformOrigin.value
+				: ['top', 'bottom'].indexOf(placement) > -1
+				? `center ${origin}`
+				: `${origin} center`
+	}
+
+	const appendArrow = (element: any) => {
+		let hash
+		if (appended.value) {
+			return
+		}
+
+		appended.value = true
+
+		for (let item in element.attributes) {
+			if (/^_v-/.test(element.attributes[item].name)) {
+				hash = element.attributes[item].name
+				break
+			}
+		}
+
+		const arrow = document.createElement('div')
+
+		if (hash) {
+			arrow.setAttribute(hash, '')
+		}
+		arrow.setAttribute('x-arrow', '')
+		arrow.className = 'popper__arrow'
+		element.appendChild(arrow)
+	}
+
+	onBeforeUnmount(() => {
+		doDestroy(true)
+		// if (popperElm && popperElm.parentNode === document.body) {
+		// 	popperElm.removeEventListener('click', stop)
+		// 	document.body.removeChild(popperElm)
+		// }
+	})
+
+	onDeactivated(() => {
+		// this.$options.beforeDestroy[0].call(this)
+	})
+}
