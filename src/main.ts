bf16cf70ee339a6942452bc875b802f1997d6909
sy <286506460@qq.com>
2020-06-24 15:11:13 +0800

    install VueNextEventPlugin

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import './theme/src/index.scss'
 import './views/demo.css'
+import VueNextEventPlugin from 'vue-next-event-plugin'
 
-createApp(App as any)
-	.use(router)
-	.mount('#app')
+const app = createApp(App as any)
+
+app.use(router)
+app.use(VueNextEventPlugin)
+app.mount('#app')
